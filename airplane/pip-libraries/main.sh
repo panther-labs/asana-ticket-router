2dcc63d2935ef389b19247e700188e2f95857e99
Nick Hibberd <nhibberd@users.noreply.github.com>
2021-11-17 15:56:01 -0800

    Pip libraries (#35)

---
+++ airplane/pip-libraries/main.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -eu
+# Linked to https://app.airplane.dev/t/set_piplibraries [do not edit this line]
+
+# Params are in environment variables as PARAM_{SLUG}, e.g. PARAM_USER_ID
+: "${PARAM_CUSTOMERID:?Expected CUSTOMERID param}"
+: "${PARAM_PIPLIBRARIES:?Expected PIPLIBRARIES param}"
+
+echo "Setup"
+eval `ssh-agent -s`
+setup-github
+REPOSITORY=hosted-deployments git-clone
+
+echo "================================"
+
+echo "Customer: ${PARAM_CUSTOMERID}"
+
+cd hosted-deployments
+
+CONFIG_FILE="deployment-metadata/deployment-targets/${PARAM_CUSTOMERID}.yml"
+
+yq e -i '.CloudFormationParameters.PipLibraries = strenv(PARAM_PIPLIBRARIES)' "${CONFIG_FILE}"
+
+git diff
+
+# TODO commit / push
