0a70dac599f7c3e9229f74744512a3668266ca0f
Tim Scott <tscott8706@users.noreply.github.com>
2021-12-10 15:57:10 -0600

    Get Panther version from generate customer files (#67)

--- airplane/dynamo-db/README.md
+++ airplane/dynamo-db/README.md
@@ -4,4 +4,4 @@ From the directory holding this README.md file:
 `./local_run.sh [path/to/script.py]`
 
 Example:
-`./local_run.sh panther_version_retriever.py`
+`./local_run.sh aws_account_id_retriever.py`
--- airplane/new-customer-deployments/create_new_customer.sh
+++ airplane/new-customer-deployments/create_new_customer.sh
@@ -11,9 +11,13 @@ printf "\nWorking in hosted deployments branch (which is an Airplane environment
 pip3 install -r automation-scripts/requirements.txt
 printf "\n\n=== Generating customer files ===\n"
 FAIRYTALE_NAME=$(python3 automation-scripts/create_config_from_airplane.py)
-echo "airplane_output_set {\"fairytale_name\": \"${FAIRYTALE_NAME}\"}"
 python3 automation-scripts/lint.py
 python3 automation-scripts/generate.py
+GEN_FILE="deployment-metadata/generated/${FAIRYTALE_NAME}.yml"
+PANTHER_TEMPLATE_URL=$(yq e '.PantherTemplateURL' "${GEN_FILE}")
+# shellcheck disable=SC2001
+PANTHER_VERSION=$(echo "${PANTHER_TEMPLATE_URL}" | sed 's%.*s3.amazonaws.com/v\(.*\)/panther.yml%\1%')
+
 printf "\nNew/changed files are:\n%s" "$(git status --porcelain | sed s/^...//)"
 printf "\n=== Finished generating customer files ===\n\n"
 
@@ -21,3 +25,4 @@ git add deployment-metadata
 TITLE="Creating customer '${PARAM_CUSTOMER_NAME}'" git-commit
 git-push
 
+echo "airplane_output_set {\"fairytale_name\": \"${FAIRYTALE_NAME}\", \"panther_version\": \"${PANTHER_VERSION}\"}"
