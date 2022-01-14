6c9aec3c628270deeba452f7505ed491d35ff72b
Nick Hibberd <nhibberd@users.noreply.github.com>
2022-01-14 15:48:49 -0800

    Handle publish race condition (#98)

--- airplane/deployment/deploy-staging/deploy_staging.sh
+++ airplane/deployment/deploy-staging/deploy_staging.sh
@@ -11,6 +11,12 @@ printf "\n\n=== Generating changes for staging deploy ===\n"
 # Find latest RC artifact
 LATEST_RC=$(aws s3 ls s3://panther-enterprise-us-west-2/v | awk '{ print $2 }' | grep RC | grep -v 'RC/' | sort -V | tail -n1 | awk -F '/' '{print $1}')
 
+PUBLISH_TIME=$(TZ=UTC aws s3 ls s3://panther-enterprise-us-west-2/${LATEST_RC}/panther.yml | | awk '{print $1"T"$2}')
+if [ "${PUBLISH_TIME}" = "" ]; then
+    echo "RC ${LATEST_RC} publish is not complete"
+    exit 1
+fi
+
 echo "Found latest RC: ${LATEST_RC}"
 export LATEST_RC
 
