456e603cd50bcc3f439352f505f703c73aa54c30
Nick Hibberd <nhibberd@users.noreply.github.com>
2022-01-14 12:07:07 -0800

    Add task to automatically update staging to the latest RC (#94)

---
+++ airplane/deployment/deploy-staging/deploy_staging.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -eu
+# Linked to https://app.airplane.dev/t/deploy_staging_smq [do not edit this line]
+
+REPOSITORY=staging-deployments git-clone
+cd staging-deployments
+git checkout main
+
+pip3 install -r automation-scripts/requirements.txt
+printf "\n\n=== Generating changes for staging deploy ===\n"
+
+# Find latest RC artifact
+LATEST_RC=$(aws s3 ls s3://panther-enterprise-us-west-2/v | awk '{ print $2 }' | grep RC | grep -v 'RC/' | sort -V | tail -n1 | awk -F '/' '{print $1}')
+
+echo "Found latest RC: ${LATEST_RC}"
+export LATEST_RC
+
+CONFIG_FILE="deployment-metadata/deployment-groups/staging.yml"
+
+yq e -i '.Version = strenv(LATEST_RC)' "${CONFIG_FILE}"
+
+git diff
+
+if $(git diff --quiet deployment-metadata); then
+    echo "No changes made"
+    exit 0
+fi
+
+# Generate and lint
+python3 automation-scripts/generate.py
+python3 automation-scripts/lint.py
+
+echo "Changes"
+git diff deployment-metadata
+
+git add deployment-metadata
+
+echo "Staged changes"
+git status
+
+TITLE="Updating staging to '${LATEST_RC}'" git-commit
+git-push
+
+echo "airplane_output_set {\"version\": \"${LATEST_RC}\"}"
