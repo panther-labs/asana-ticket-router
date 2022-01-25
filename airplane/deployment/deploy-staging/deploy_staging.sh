a4cc1a7a383a624b5e975d9ff09c0c58b62aa20e
Nick Hibberd <nhibberd@users.noreply.github.com>
2022-01-24 16:24:49 -0800

    Update deploy staging to upgrade internal hosted accounts (#106)

--- airplane/deployment/deploy-staging/deploy_staging.sh
+++ airplane/deployment/deploy-staging/deploy_staging.sh
@@ -1,13 +1,6 @@
 #!/bin/sh -eu
 # Linked to https://app.airplane.dev/t/deploy_staging_smq [do not edit this line]
 
-REPOSITORY=staging-deployments git-clone
-cd staging-deployments
-git checkout main
-
-pip3 install -r automation-scripts/requirements.txt
-printf "\n\n=== Generating changes for staging deploy ===\n"
-
 # Find latest RC artifact
 LATEST_RC=$(aws s3 ls s3://panther-enterprise-us-west-2/v | awk '{ print $2 }' | grep RC | grep -v 'RC/' | sort -V | tail -n1 | awk -F '/' '{print $1}')
 
@@ -20,11 +13,16 @@ fi
 echo "Found latest RC: ${LATEST_RC}"
 export LATEST_RC
 
+## staging-deployment
+printf "\n\n=== Generating changes for staging-deployments ===\n"
+REPOSITORY=staging-deployments git-clone
+cd staging-deployments
+git checkout main
+
+pip3 install -r automation-scripts/requirements.txt
+
 CONFIG_FILE="deployment-metadata/deployment-groups/staging.yml"
-
 yq e -i '.Version = strenv(LATEST_RC)' "${CONFIG_FILE}"
-
-git diff
 
 if $(git diff --quiet deployment-metadata); then
     echo "No changes made"
@@ -37,7 +35,6 @@ python3 automation-scripts/lint.py
 
 echo "Changes"
 git diff deployment-metadata
-
 git add deployment-metadata
 
 echo "Staged changes"
@@ -45,5 +42,39 @@ git status
 
 TITLE="Updating staging to '${LATEST_RC}'" git-commit
 TEST_RUN=false git-push
+
+cd -
+
+## hosted-deployment
+printf "\n\n=== Generating changes for hosted-deployments ===\n"
+REPOSITORY=hosted-deployments git-clone
+cd hosted-deployments
+git checkout main
+
+pip3 install -r automation-scripts/requirements.txt
+
+CONFIG_FILE="deployment-metadata/deployment-groups/internal.yml"
+yq e -i '.Version = strenv(LATEST_RC)' "${CONFIG_FILE}"
+
+if $(git diff --quiet deployment-metadata); then
+    echo "No changes made"
+    exit 0
+fi
+
+# Generate and lint
+python3 automation-scripts/generate.py
+python3 automation-scripts/lint.py
+
+echo "Changes"
+git diff deployment-metadata
+git add deployment-metadata
+
+echo "Staged changes"
+git status
+
+TITLE="Updating internal group to '${LATEST_RC}'" git-commit
+TEST_RUN=false git-push
+
+## Airplane output
 
 echo "airplane_output_set {\"version\": \"${LATEST_RC}\"}"
