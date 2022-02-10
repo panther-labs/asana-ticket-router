1d53ac65d4d39f2ee925b720f0ea79a71b022106
Tim Scott <tscott8706@users.noreply.github.com>
2022-02-10 10:08:24 -0600

    Create Remove Hosted Delpoyment Files AP task (#135)
    Co-authored-by: Dennis Webb <dhwebb@gmail.com>

--- airplane/pyshared/git_ops.py
+++ airplane/pyshared/git_ops.py
@@ -12,28 +12,23 @@ def _raise_runtime_exception(e):
 def setup_github():
     """Requires the DEPLOY_KEY_BASE64 environment variable to be set in the Airplane task."""
     try:
-        subprocess.check_output("util/setup-github",
-                                shell=True,
-                                stderr=subprocess.STDOUT)
+        subprocess.check_output("util/setup-github", shell=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         _raise_runtime_exception(e)
 
 
 def git_add(files: List[str]):
     try:
-        subprocess.check_output(f'{_UTIL_PATH}/git-add {" ".join(files)}',
-                                shell=True,
-                                stderr=subprocess.STDOUT)
+        subprocess.check_output(f'{_UTIL_PATH}/git-add {" ".join(files)}', shell=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         _raise_runtime_exception(e)
 
 
 def git_checkout(branch, create=False):
     try:
-        subprocess.check_output(
-            f"BRANCH={branch} CREATE={str(create).lower()} {_UTIL_PATH}/git-checkout",
-            shell=True,
-            stderr=subprocess.STDOUT)
+        subprocess.check_output(f"BRANCH={branch} CREATE={str(create).lower()} {_UTIL_PATH}/git-checkout",
+                                shell=True,
+                                stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         _raise_runtime_exception(e)
 
@@ -42,9 +37,7 @@ def git_clone(repo, github_setup=False):
     if github_setup:
         setup_github()
     try:
-        subprocess.check_output(f"REPOSITORY={repo} {_UTIL_PATH}/git-clone",
-                                shell=True,
-                                stderr=subprocess.STDOUT)
+        subprocess.check_output(f"REPOSITORY={repo} {_UTIL_PATH}/git-clone", shell=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         _raise_runtime_exception(e)
     return repo
@@ -52,19 +45,25 @@ def git_clone(repo, github_setup=False):
 
 def git_commit(title, description=""):
     try:
-        subprocess.check_output(
-            f'TITLE="{title}" DESCRIPTION="{description}" {_UTIL_PATH}/git-commit',
-            shell=True,
-            stderr=subprocess.STDOUT)
+        subprocess.check_output(f'TITLE="{title}" DESCRIPTION="{description}" {_UTIL_PATH}/git-commit',
+                                shell=True,
+                                stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         _raise_runtime_exception(e)
 
 
 def git_push(test_run=False):
     try:
-        subprocess.check_output(
-            f'TEST_RUN="{str(test_run).lower()}" {_UTIL_PATH}/git-push',
-            shell=True,
-            stderr=subprocess.STDOUT)
+        output = subprocess.check_output(f'TEST_RUN="{str(test_run).lower()}" {_UTIL_PATH}/git-push',
+                                         shell=True,
+                                         stderr=subprocess.STDOUT)
+        if output:
+            print(output.decode())
     except subprocess.CalledProcessError as e:
         _raise_runtime_exception(e)
+
+
+def git_add_commit_push(files: List[str], title, description="", test_run=False):
+    git_add(files=files)
+    git_commit(title=title, description=description)
+    git_push(test_run=test_run)
