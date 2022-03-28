adf3579340769c4d77358fbab4d876e22568f74d
George Papadrosou <giorgosp@users.noreply.github.com>
2022-03-28 16:25:47 +0300

    Route panther-rbac-datalake-sync lambda errors to Ingestion team (#163)

--- sentry_asana/src/consumer/components/asana/entities.py
+++ sentry_asana/src/consumer/components/asana/entities.py
@@ -170,6 +170,7 @@ class SERVICE(Enum):
     OUTPUTS_API = 'panther-outputs-api'
     PIP_LAYER_BUILDER = 'panther-pip-layer-builder'
     POLICY_ENGINE = 'panther-policy-engine'
+    RBAC_DATALAKE_SYNC = 'panther-rbac-datalake-sync'
     REPLAY_API = 'panther-replay-api'
     REPLAY_LOG_PUSHER = 'panther-replay-log-pusher'
     RESOURCES_API = 'panther-resources-api'
@@ -217,6 +218,7 @@ _TEAM_TO_SERVICE: Dict[TEAM, List[SERVICE]] = {
         SERVICE.RULES_ENGINE,
         SERVICE.SOURCE_API,
         SERVICE.SYSTEM_STATUS,
+        SERVICE.RBAC_DATALAKE_SYNC
     ],
     TEAM.INVESTIGATIONS: [
         SERVICE.ALERTS_API,
