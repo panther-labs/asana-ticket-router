ef9d03606529b00bb3490b11be5c823d449e1404
Tim Scott <tscott8706@users.noreply.github.com>
2022-04-15 17:18:23 -0500

    Airplane - Add ability to remove from vault config (#184)

--- airplane/operations/vault-cfg/update_vault_config.sh
+++ airplane/operations/vault-cfg/update_vault_config.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Linked to https://app.airplane.dev/t/update_vault_config [do not edit this line]
 set -eu
 
 PATH=$PATH:util # For running airplane locally
@@ -8,15 +7,23 @@ cd aws-vault-config
 
 pip install -e .
 
-args="--name ${PARAM_FAIRYTALE_NAME} --account-id ${PARAM_AWS_ACCOUNT_ID} --region ${PARAM_REGION} \
-  --deployment-type ${PARAM_SERVICE_TYPE}"
+if [ "${PARAM_ADD_OR_REMOVE}" = "Add" ]; then
+  # Only need to check account ID, as other params are required by Airplane or are lists with a default
+  if [ -z "${PARAM_AWS_ACCOUNT_ID}" ]; then echo "AWS Account ID is required"; exit 1; fi
 
-if [ "${PARAM_SERVICE_TYPE}" = "CPaaS" ]; then
-  args="${args} --profile-types deployment support"
+  args="--name ${PARAM_FAIRYTALE_NAME} --account-id ${PARAM_AWS_ACCOUNT_ID} --region ${PARAM_REGION} \
+    --deployment-type ${PARAM_SERVICE_TYPE}"
+
+  if [ "${PARAM_SERVICE_TYPE}" = "CPaaS" ]; then
+    args="${args} --profile-types deployment support"
+  fi
+  aws-vault-config add-customer $(echo ${args})
+else
+  args="--name ${PARAM_FAIRYTALE_NAME}"
+  aws-vault-config remove-customer $(echo ${args})
 fi
 
-aws-vault-config add-customer $(echo ${args})
 
 git add aws_vault_config/aws_config.yml
-TITLE="Add ${PARAM_FAIRYTALE_NAME} profiles" git-commit
+TITLE="${PARAM_ADD_OR_REMOVE} ${PARAM_FAIRYTALE_NAME} profiles" git-commit
 TEST_RUN="${PARAM_AIRPLANE_TEST_RUN}" git-push
