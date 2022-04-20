01afa2fcdf71e66c8513bd50d1eccea41c1afd3b
Mykola (Nick) Demianchuk <mykola.demianchuk@panther.io>
2022-04-20 09:53:57 -0500

    Add AP task to delete customer panther master stack (#178)
    * Add AP task to delete customer panther master stack
    
    * Use Customer Support as initial role
    
    * Use CustomerTeardown role
    
    * Remove legacy airplane task link
    
    * Address Tim's comments
    
    * Disable self approvals
    
    * Update customer teardown role ARN
    
    * Remove base class of DeploymentsRepo
    
    * Code cleanup
    
    * Update get_deployment_filepath function

---
+++ airplane/operations/deprovisions/delete-customer-master-stack/delete_customer_master_stack.py
@@ -0,0 +1,57 @@
+import os
+
+from pyshared.aws_creds import get_credentialed_resource, get_credentialed_client
+from pyshared.aws_consts import get_aws_const
+from pyshared.deployments_file import get_deployment_filepath, DeploymentsRepo
+from pyshared.git_ops import git_clone
+from pyshared.yaml_utils import load_yaml_cfg
+
+DEFAULT_MASTER_STACK_NAME = "panther"
+CUSTOMER_TEARDOWN_ROLE_ARN = get_aws_const(const_name="CUSTOMER_TEARDOWN_ROLE_ARN")
+
+
+def get_customer_teardown_role_arns(aws_account_id: str) -> tuple[str, str]:
+    return CUSTOMER_TEARDOWN_ROLE_ARN, f"arn:aws:iam::{aws_account_id}:role/PantherTeardownRole"
+
+
+def get_master_stack_name(fairytale_name: str, repo_dir: str) -> str:
+    deployment_file = get_deployment_filepath(fairytale_name=fairytale_name, repo_dir=repo_dir)
+    cfn_yaml = load_yaml_cfg(cfg_filepath=deployment_file,
+                             error_msg=f"Customer deployment file not found: '{deployment_file}'")
+    return cfn_yaml.get("PantherStackName", DEFAULT_MASTER_STACK_NAME)
+
+
+def delete_master_stack(params: dict, stack_name: str, test_run: bool) -> None:
+    cfn_kwargs = {
+        "service_name": "cloudformation",
+        "arns": get_customer_teardown_role_arns(params["aws_account_id"]),
+        "desc": f"cfn_remove_{params['fairytale_name']}_master_stack",
+        "region": params["region"]
+    }
+    stack = get_credentialed_resource(**cfn_kwargs).Stack(stack_name)
+
+    if test_run:
+        print(f"Test run, not deleting the '{stack.name}' stack.")
+        return
+
+    # Delete the stack
+    stack.delete()
+    print(f"Initiated '{stack_name}' stack deletion.")
+
+    # The waiter will make at most 60 attempts with 60s interval in between
+    waiter_cfg = {"MaxAttempts": 60, "Delay": 60}
+
+    # Wait for status 'stack_delete_complete'
+    get_credentialed_client(**cfn_kwargs) \
+        .get_waiter("stack_delete_complete") \
+        .wait(StackName=stack_name, WaiterConfig=waiter_cfg)
+
+    print(f"Deleted the '{stack_name}' stack")
+
+
+def main(params: dict) -> None:
+    hosted_deploy_dir = git_clone(repo=DeploymentsRepo.HOSTED,
+                                  github_setup=True,
+                                  existing_dir=params.get("hosted_deploy_dir"))
+    master_stack_name = get_master_stack_name(fairytale_name=params["fairytale_name"], repo_dir=hosted_deploy_dir)
+    delete_master_stack(params=params, stack_name=master_stack_name, test_run=params["airplane_test_run"])
--- airplane/pyshared/deployments_file.py
+++ airplane/pyshared/deployments_file.py
@@ -1,20 +1,20 @@
-from collections.abc import Callable
-from enum import Enum
+import os
 from typing import Dict
+from collections.abc import Callable
 
 from pyshared.git_ops import git_clone, git_add_commit_push
 from pyshared.load_py_file import load_py_file_as_module
 from pyshared.os_utils import tmp_change_dir
 
 
-class DeploymentsRepo(Enum):
+class DeploymentsRepo:
     HOSTED = "hosted-deployments"
     STAGING = "staging-deployments"
 
 
-def get_deployment_filepath(fairytale_name, get_generated_filepath=False):
+def get_deployment_filepath(fairytale_name: str, repo_dir: str = "", get_generated_filepath: bool = False) -> str:
     parent_dir = "deployment-targets" if not get_generated_filepath else "generated"
-    return f"deployment-metadata/{parent_dir}/{fairytale_name}.yml"
+    return os.path.join(repo_dir, "deployment-metadata", parent_dir, f"{fairytale_name}.yml")
 
 
 def gen_cfgs():
@@ -23,22 +23,22 @@ def gen_cfgs():
     generate_mod.generate_configs()
 
 
-def alter_deployment_file(deployments_repo: DeploymentsRepo,
+def alter_deployment_file(deployments_repo: str,
                           ap_params: Dict[str, str],
                           alter_callable: Callable[str],
                           commit_title: str,
                           apply_to_generated_file: bool = False):
     """Alter a deployment file in some way. The alter_callable is a function that, given the deployment file, will
     alter it."""
-    deploy_dir = git_clone(repo=deployments_repo.value,
+    deploy_dir = git_clone(repo=deployments_repo,
                            github_setup=True,
-                           existing_dir=ap_params.get(deployments_repo.value))
+                           existing_dir=ap_params.get(deployments_repo))
     with tmp_change_dir(change_dir=deploy_dir):
         alter_callable(get_deployment_filepath(fairytale_name=ap_params["fairytale_name"]))
         if apply_to_generated_file:
             alter_callable(
                 get_deployment_filepath(fairytale_name=ap_params["fairytale_name"], get_generated_filepath=True))
         gen_cfgs()
-        git_add_commit_push(files=("deployment-metadata", ),
+        git_add_commit_push(files=("deployment-metadata",),
                             title=commit_title,
                             test_run=ap_params["airplane_test_run"])
