243973e227b040d2946ca3c12d81fe85b658a568
Tim Scott <tscott8706@users.noreply.github.com>
2022-06-28 14:36:58 -0500

    Notify security when account is being deleted (#308)

--- airplane/operations/deprovisions/delete-dns-records/delete_dns_records.py
+++ airplane/operations/deprovisions/delete-dns-records/delete_dns_records.py
@@ -1,37 +1,58 @@
-from pyshared.aws_consts import get_aws_const
-from pyshared.aws_creds import get_credentialed_client
 from botocore.exceptions import ClientError
 
-
-def get_delete_stack_role_arn(organization: str) -> str:
-    if organization == "root":
-        return get_aws_const("ROOT_CFN_DELETE_STACK_ROLE_ARN")
-    elif organization == "hosted":
-        return get_aws_const("HOSTED_CFN_DELETE_STACK_ROLE_ARN")
-    raise AttributeError(f"Role doesn't exist in {organization}.")
+from pyshared.aws_consts import get_aws_const
+from pyshared.aws_creds import get_credentialed_client
+from pyshared.customer_info_retriever import AllCustomerAccountsInfo
+from v2.consts.airplane_env import AirplaneEnv
+from v2.task_models.airplane_task import AirplaneTask
 
 
-def delete_dns_records(organization: str, fairytale_name: str) -> None:
-    cfn_kwargs = {
-        "service_name": "cloudformation",
-        "arns": get_delete_stack_role_arn(organization),
-        "desc": f"cfn_remove_dns_records",
-        "region": "us-west-2"
-    }
-    cfn_client = get_credentialed_client(**cfn_kwargs)
-    cfn_waiter = cfn_client.get_waiter("stack_delete_complete")
+class DnsRecordRemover(AirplaneTask):
 
-    stack_names = [f"route53-{fairytale_name}", f"panther-cert-{fairytale_name}"]
-    for stack_name in stack_names:
-        try:
-            # Try to find the stack by name, raises an error if doesn't exist
-            cfn_client.describe_stacks(StackName=stack_name)
-            cfn_client.delete_stack(StackName=stack_name)
-            cfn_waiter.wait(StackName=stack_name, WaiterConfig={"Delay": 30, "MaxAttempts": 20})
-            print(f"Deleted '{stack_name}' stack.")
-        except ClientError:
-            print(f"Stack '{stack_name}' doesn't exist or is already deleted.")
+    @staticmethod
+    def get_delete_stack_role_arn(organization: str) -> str:
+        if organization == "root":
+            return get_aws_const("ROOT_CFN_DELETE_STACK_ROLE_ARN")
+        elif organization == "hosted":
+            return get_aws_const("HOSTED_CFN_DELETE_STACK_ROLE_ARN")
+        raise AttributeError(f"Role doesn't exist in {organization}.")
+
+    def delete_dns_records(self, organization: str, fairytale_name: str) -> bool:
+        cfn_kwargs = {
+            "service_name": "cloudformation",
+            "arns": self.get_delete_stack_role_arn(organization),
+            "desc": f"cfn_remove_dns_records",
+            "region": "us-west-2"
+        }
+        cfn_client = get_credentialed_client(**cfn_kwargs)
+        cfn_waiter = cfn_client.get_waiter("stack_delete_complete")
+
+        is_stack_deleted = False
+        stack_names = [f"route53-{fairytale_name}", f"panther-cert-{fairytale_name}"]
+        for stack_name in stack_names:
+            try:
+                # Try to find the stack by name, raises an error if doesn't exist
+                cfn_client.describe_stacks(StackName=stack_name)
+                cfn_client.delete_stack(StackName=stack_name)
+                cfn_waiter.wait(StackName=stack_name, WaiterConfig={"Delay": 30, "MaxAttempts": 20})
+                print(f"Deleted '{stack_name}' stack.")
+                is_stack_deleted = True
+            except ClientError:
+                print(f"Stack '{stack_name}' doesn't exist or is already deleted.")
+
+        return is_stack_deleted
+
+    def run(self, params):
+        fairytale_name = params["fairytale_name"]
+        is_stack_deleted = self.delete_dns_records(organization=params["organization"], fairytale_name=fairytale_name)
+        if is_stack_deleted:
+            aws_account_id = AllCustomerAccountsInfo.get_notion_results()[fairytale_name].AWS_Account_ID
+            self.send_slack_message(
+                channel_name="#security-alerts-high-pri",
+                message=f"Account {aws_account_id}/{fairytale_name} is being deprovisioned "
+                f"(see {AirplaneEnv.get_task_run_url()}).\n\n"
+                f"Requestor email: {AirplaneEnv.REQUESTOR_EMAIL}, team ID: {AirplaneEnv.AIRPLANE_TEAM_ID}")
 
 
 def main(params):
-    delete_dns_records(organization=params["organization"], fairytale_name=params["fairytale_name"])
+    DnsRecordRemover().run(params)
--- airplane/v2/consts/airplane_env.py
+++ airplane/v2/consts/airplane_env.py
@@ -6,10 +6,12 @@ class AirplaneEnv:
     AIRPLANE_ENV = os.getenv("AIRPLANE_ENV")
     AIRPLANE_SCHEDULE_ID = os.getenv("AIRPLANE_SCHEDULE_ID")
     AIRPLANE_TASK_ID = os.getenv("AIRPLANE_TASK_ID")
+    AIRPLANE_REQUESTER_EMAIL = os.getenv("AIRPLANE_REQUESTER_EMAIL")
     AIRPLANE_RUNNER_ID = os.getenv("AIRPLANE_RUNNER_ID")
     AIRPLANE_RUNNER_EMAIL = os.getenv("AIRPLANE_RUNNER_EMAIL")
     AIRPLANE_RUN_ID = os.getenv("AIRPLANE_RUN_ID")
     AIRPLANE_SESSION_ID = os.getenv("AIRPLANE_SESSION_ID")
+    AIRPLANE_TEAM_ID = os.getenv("AIRPLANE_TEAM_ID")
 
     @classmethod
     def is_local_env(cls):
