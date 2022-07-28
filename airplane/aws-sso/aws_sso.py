c01d1ae228e270dfb59b199e17bdb7aca69e56ee
stedrow <104793655+stedrow@users.noreply.github.com>
2022-07-28 12:07:54 -0400

    updated permission set list & move user_id to airplane requester email address (#336)
    * updated permission set list and removed user_id field to pull from airplane environment vairable for email address of requester.
    
    * added check for AIRPLANE_REQUESTER_EMAIL environment variable
    
    * removed f-string from line where it was not needed.

--- airplane/aws-sso/aws_sso.py
+++ airplane/aws-sso/aws_sso.py
@@ -1,9 +1,10 @@
+import os
 from pyshared.dynamo_db import DynamoDbSearch, get_ddb_table
 from pyshared.date_utils import generate_utc_timestamp, generate_utc_expiry_timestamp
 
 
 def write_to_db(table, data_to_write):
-    extra_fields = {'request_time': generate_utc_timestamp(), 'request_status': 'NEW'}
+    extra_fields = {'user_id': os.getenv("AIRPLANE_REQUESTER_EMAIL"), 'request_time': generate_utc_timestamp(), 'request_status': 'NEW'}
     response = table.put_item(Item={**data_to_write, **extra_fields})
     return response
 
@@ -11,21 +12,30 @@ def write_to_db(table, data_to_write):
 def check_valid_expiry_time(expiry_time):
     if not 1 <= int(expiry_time) <= 4:
         raise ValueError(
-                    f"Invalid expiry time provided: {expiry_time} not one of the valid values of [1,2,3,4]"
+                    f"Invalid expiry time provided: {expiry_time} not one of the valid values (1 - 4)"
                 )
 
 
 def check_valid_permission_set(permission_set):
-    possible_permission_set = {'panther-ops', 'panther-viewonly', 'panther-billing', 'panther-billingadmin', 'panther-readsfsecret'}
+    possible_permission_set = {'panther-ops', 'panther-viewonly', 'panther-billing', 'panther-billingadmin', 'panther-readsfsecret', 
+    'panther-dataadmin', 'panther-deployadmin', 'panther-orgadmin'}
     if permission_set not in possible_permission_set:
         raise ValueError(
                     f"Invalid permission set was specified: {permission_set}"
                 )
 
 
+def check_valid_env_airplane_requestor_email():
+    if os.environ.get('AIRPLANE_REQUESTER_EMAIL') is None:
+        raise ValueError(
+            "AIRPLANE_REQUESTER_EMAIL environment variable not set, unable to complete task since no email address was supplied."
+        )
+
+
 def main(params):
     check_valid_expiry_time(params['expiry_time'])
     check_valid_permission_set(params['permission_set'])
+    check_valid_env_airplane_requestor_email()
 
     table_name = "escalation-requests"
     role_arn = "arn:aws:iam::945083323154:role/AirplaneDynamoDBWrite"
