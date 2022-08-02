a85d4737ae7b7323928ef66f96d4e13b0e64bf22
Alec Warner <alec.warner@panther.io>
2022-08-02 15:44:08 -0700

    Add a first cut of entities module.
     - Dataclasses for teams and services; mostly modeled after existing code.
     - Entities service to offer lookups.
    
    TODO: More dependency injection around logging and file IO, potentially.
    
    Nit: Due to the way the dataclass wizard works; I opted to not extract it from entities..so its tightly bound
    and not dep injected..so we don't use the serialized provider for example.
    
    In theory we could, I supposed, have some kind of dataclass injector (that injects JSON or YamlWizard dataclass types?) and extracts that interface; but honestly it looks like
    more work than its worth.
    
    Signed-off-by: Alec Warner <alec.warner@panther.io>

---
+++ sentry_asana/src/common/components/entities/containers.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2022 Panther Labs Inc
+#
+# Panther Enterprise is licensed under the terms of a commercial license available from
+# Panther Labs Inc ("Panther Commercial License") by contacting contact@runpanther.com.
+# All use, distribution, and/or modification of this software, whether commercial or non-commercial,
+# falls under the Panther Commercial License to the extent it is permitted.
+
+from dependency_injector import containers, providers
+from . import service
+
+class EntitiesContainer(containers.DeclarativeContainer):
+    """Entities Container"""
+
+    logger = providers.Dependency()
+    teams_service = providers.Singleton(
+      service.TeamService,
+      logger=logger,
+    )
---
+++ sentry_asana/src/tests/test_entities.py
@@ -0,0 +1,28 @@
+import asyncio
+import pytest
+from unittest import mock
+
+from ..common.components.secrets.service import SecretsManagerService
+from ..common.components.secrets.containers import SecretsManagerContainer
+from ..common.components.serializer.service import SerializerService
+from ..common.components.serializer.containers import SerializerContainer
+from ..common.components.logger.service import LoggerService
+from ..common.components.logger.containers import LoggerContainer
+from ..consumer.components.application import ApplicationContainer
+from ..consumer.components.sentry.containers import SentryContainer
+from ..consumer.components.requests.containers import RequestsContainer
+from ..consumer.components.sentry.service import SentryService
+from ..consumer.components.asana.containers import AsanaContainer
+from ..consumer.components.asana.service import AsanaService
+
+from ..common.components.entities.service import TeamService
+from ..common.components.entities.containers import EntitiesContainer
+
+
+@pytest.fixture
+def container_with_mock() -> EntitiesContainer:
+  return EntitiesContainer(logger=LoggerContainer().logger)
+
+def test_TeamsServiceInit(container_with_mock: EntitiesContainer):
+  teams_service = container_with_mock.teams_service()
+  assert teams_service.DefaultService() != []
