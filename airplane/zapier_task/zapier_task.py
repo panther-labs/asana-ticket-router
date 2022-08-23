7d83e8a0a4a742ad101259d1924642818f73819e
BJ Maldonado <bj.maldonado@panther.io>
2022-08-23 16:59:50 -0400

    Added fields we wanna be expecting from Salesforce

--- airplane/zapier_task/zapier_task.py
+++ airplane/zapier_task/zapier_task.py
@@ -1,5 +1,3 @@
-import os
-
 from v2.consts.airplane_env import AirplaneEnv
 
 
@@ -9,4 +7,3 @@ def main(params: dict):
         print(f"Ha, you got zapped nerd: {username}")
     else:
         print(f"You still got zapped non-api nerd! {username}")
-
