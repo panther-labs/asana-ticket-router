7cadc3d2e58be24759342c1cdf953f5c497d022d
BJ Maldonado <bj.maldonado@panther.io>
2022-08-23 17:04:21 -0400

    For now its just gonna print out the paramsg

--- airplane/zapier_task/zapier_task.py
+++ airplane/zapier_task/zapier_task.py
@@ -2,8 +2,7 @@ from v2.consts.airplane_env import AirplaneEnv
 
 
 def main(params: dict):
-    username = params.get("username", "DefaultedInTask")
     if AirplaneEnv.is_api_user_execution():
-        print(f"Ha, you got zapped nerd: {username}")
+        print(f"Ha, you got zapped nerd: {params}")
     else:
-        print(f"You still got zapped non-api nerd! {username}")
+        print(f"You still got zapped non-api nerd! {params}")
