ae6df1352fdcb2026577437a41dbbadce1f54ed2
Tim Scott <tscott8706@users.noreply.github.com>
2022-08-29 15:06:50 -0500

    Fix checking for deprovs (#426)

--- airplane/pyshared/deprov_info.py
+++ airplane/pyshared/deprov_info.py
@@ -20,9 +20,17 @@ class DeprovInfoDeployFile:
         self.filepath = filepath
 
     def retrieve_deprov_info(self) -> DeprovInfo:
-        cfg = load_yaml_cfg(self.filepath)
+        cfg = load_yaml_cfg(self.filepath, load_tz=True)
         return DeprovInfo(**cfg.get(self.CFG_KEY, {}))
+
+    def remove_deprov_info(self):
+        with change_yaml_file(self.filepath) as cfg:
+            cfg.pop(self.CFG_KEY, None)
 
     def write_deprov_info(self, deprov_info: DeprovInfo):
         with change_yaml_file(self.filepath) as cfg:
             cfg[self.CFG_KEY] = asdict(deprov_info)
+
+    def dns_removed(self):
+        info = self.retrieve_deprov_info()
+        return (info.teardown_time is not None) and (info.dns_removal_time is None)
--- airplane/pyshared/yaml_utils.py
+++ airplane/pyshared/yaml_utils.py
@@ -3,18 +3,24 @@ from contextlib import contextmanager
 from ruamel.yaml import YAML, comments
 
 
-def _yaml_instance() -> YAML:
+def _timestamp_constructor(loader, node):
+    return dateutil.parser.parse(node.value)
+
+
+def _yaml_instance(include_tz_in_timestamps=False) -> YAML:
     yaml = YAML(pure=True)
+    if include_tz_in_timestamps:
+        yaml.constructor.add_constructor(u'tag:yaml.org,2002:timestamp', _timestamp_constructor)
     yaml.indent(mapping=2, sequence=4, offset=2)
     return yaml
 
 
-def load_yaml_cfg(cfg_filepath: str, error_msg: str = None) -> comments.CommentedMap:
+def load_yaml_cfg(cfg_filepath: str, error_msg: str = None, load_tz=False) -> comments.CommentedMap:
     error_msg = error_msg if (error_msg is not None) else f"Could not find `{cfg_filepath}'"
 
     try:
         with open(cfg_filepath, "r") as cfg_file:
-            return _yaml_instance().load(cfg_file)
+            return _yaml_instance(include_tz_in_timestamps=load_tz).load(cfg_file)
     except FileNotFoundError:
         raise ValueError(error_msg)
 
