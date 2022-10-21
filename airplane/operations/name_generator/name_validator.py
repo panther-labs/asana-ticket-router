a74b9c6fe27780b78971f75238e2bcc09d786a7c
Tim Scott <tscott8706@users.noreply.github.com>
2022-10-21 08:31:28 -0500

    Use route53 not CFN to prevent AWS throttling (#522)

--- airplane/operations/name_generator/name_validator.py
+++ airplane/operations/name_generator/name_validator.py
@@ -1,5 +1,7 @@
-from pyshared import cloudformation_yaml
+from botocore.exceptions import ClientError
+
 from pyshared.aws_consts import get_aws_const
+from pyshared.aws_creds import get_credentialed_client
 from pyshared.dynamo_db import DynamoDbSearch
 from v2.exceptions import DomainNameAlreadyInUseException, FairytaleNameAlreadyInUseException
 
@@ -9,17 +11,31 @@ class NameValidator:
     DDB_METADATA_TABLE = get_aws_const("HOSTED_DEPLOYMENTS_METADATA")
     DDB_RO_ROLE_ARN = get_aws_const("HOSTED_DYNAMO_RO_ROLE_ARN")
 
-    def __init__(self, active_domain_names=None):
-        self.active_domain_names = self._populate_domain_names_in_use(
-        ) if active_domain_names is None else active_domain_names
+    def __init__(self):
         self.ddb_search = None
 
     def _get_ddb_search_instance(self):
         return self.ddb_search if self.ddb_search else DynamoDbSearch(table_name=self.DDB_METADATA_TABLE,
                                                                       arn=self.DDB_RO_ROLE_ARN)
 
+    @staticmethod
+    def _does_route53_record_exist_with_domain_name(domain_name):
+        """Starting the record name and limiting to one will do one of two things:
+            If the record exists, it will get that record, and that will be the one retrieved (and we raise an error)
+            If the record doesn't exist, it will get a random one that won't match our domain name
+        """
+        client = get_credentialed_client(service_name="route53",
+                                         arns=get_aws_const("HOSTED_ROUTE53_RO_ROLE_ARN"),
+                                         desc=f"checking_route53_domains")
+
+        record_name = client.list_resource_record_sets(HostedZoneId=get_aws_const("RUN_PANTHER_HOSTED_ZONE"),
+                                                       StartRecordName=domain_name,
+                                                       MaxItems="1")["ResourceRecordSets"][0]["Name"]
+
+        return domain_name in record_name
+
     def _validate_domain_name(self, domain_name):
-        if domain_name in self.active_domain_names:
+        if self._does_route53_record_exist_with_domain_name(domain_name):
             raise DomainNameAlreadyInUseException(f"ERROR: Domain name [{domain_name}] already in use!")
 
     def _validate_fairytale_name(self, fairytale_name):
@@ -33,11 +49,3 @@ class NameValidator:
         self._validate_fairytale_name(fairytale_name)
         print(f"INFO: Successfully validated domain [{domain_name}] and fairytale name [{fairytale_name}]!")
         return True
-
-    def _get_domain_name_from_stack(self, stack):
-        return cloudformation_yaml.get_cloudformation_physical_resource_id_from_stack(
-            role_arn=self.CFN_RO_ROLE_ARN, stack_name=stack["StackName"], logical_resource_id="CustomerSubdomain")
-
-    def _populate_domain_names_in_use(self):
-        all_active_stacks = cloudformation_yaml.get_active_customer_instance_domain_stacks(self.CFN_RO_ROLE_ARN)
-        return [self._get_domain_name_from_stack(stack) for stack in all_active_stacks]
--- airplane/tests/test_name_validator.py
+++ airplane/tests/test_name_validator.py
@@ -9,11 +9,15 @@ from v2.exceptions import DomainNameAlreadyInUseException, FairytaleNameAlreadyI
 @mock.patch("pyshared.dynamo_db.DynamoDbSearch")
 def validator(mock_ddb_search):
     mock_ddb_search = mock_ddb_search.return_value
-    validator = NameValidator(
-        active_domain_names=["used.runpanther.net", "inuse.runpanther.net", "exists.runpanther.net"])
+    validator = NameValidator()
     validator.ddb_search = mock_ddb_search
     validator.ddb_search.get_query_item.return_value = {}
+    set_route53_record_found(validator, False)
     return validator
+
+
+def set_route53_record_found(validator: NameValidator, found: bool):
+    validator._does_route53_record_exist_with_domain_name = lambda *args, **kwargs: found
 
 
 class TestNameValidator:
@@ -25,6 +29,7 @@ class TestNameValidator:
 
     @staticmethod
     def test_domain_name_already_in_use(validator):
+        set_route53_record_found(validator, True)
         with pytest.raises(DomainNameAlreadyInUseException):
             validator.run_validation(domain_name="used.runpanther.net", fairytale_name="valid-fairytale")
 
